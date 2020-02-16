5b28d78b499c6cdde136ccbc7abd4c0958454096
David Harris <dhharris9@gmail.com>
2020-02-15 23:21:42 -0800

    clang-format helpers.c

--- src/helpers.c
+++ src/helpers.c
@@ -3,9 +3,9 @@
 Image get_sprite_from_sheet(Image sheet, Vector2 index)
 {
         return ImageFromImage(sheet, (Rectangle){
-                index.x * TILE_SIZE,
-                index.y * TILE_SIZE,
-                (index.x + 1) * TILE_SIZE,
-                (index.y + 1) * TILE_SIZE,
-        });
+                                         index.x * TILE_SIZE,
+                                         index.y * TILE_SIZE,
+                                         (index.x + 1) * TILE_SIZE,
+                                         (index.y + 1) * TILE_SIZE,
+                                     });
 }
