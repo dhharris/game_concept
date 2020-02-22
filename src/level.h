55110a4c2954d8acdc6e58a7cff8cf01e0d502bc
David Harris <dhharris9@gmail.com>
2020-02-21 22:55:44 -0800

    move tile types to new file

--- src/level.h
+++ src/level.h
@@ -2,34 +2,14 @@
 
 #include "common.h"
 #include "item.h"
+#include "tiletypes.h"
 
 #define LEVEL_NAME_LIMIT 32
+#define LEVEL_SIZE 20
+
 #define NUM_LEVELS 2
-
-#define TILETYPE_NONE 0
-#define TILETYPE_AIR 1
-
-#define TILETYPE_DIRT 2
-#define TILETYPE_DIRT_ROCK 3
-#define TILETYPE_DIRT_OVERHANG 4
-
-#define TILETYPE_SURFACE 5
-#define TILETYPE_GRASS 6
-#define TILETYPE_GRASS_ROCK1 7
-#define TILETYPE_GRASS_ROCK2 8
-
-#define TILETYPE_TREE 9
-#define TILETYPE_TREE_PINE 10
-#define TILETYPE_TREE_DEAD 11
-
-#define TILETYPE_BRICK 12
-#define TILETYPE_DOOR 13
-#define TILETYPE_DOOR_OPEN 14
-#define TILETYPE_STAIRS_UP 15
-#define TILETYPE_STAIRS_DOWN 16
-#define TILETYPE_COBWEB 17
-
-#define NUM_TILE_TYPES 18 // Always one more than the last tiletype
+#define MAX_WIDTH 512
+#define MAX_HEIGHT 512
 
 typedef struct {
         int num_tile_sets;
@@ -41,10 +21,6 @@ typedef struct {
         char name[LEVEL_NAME_LIMIT];
         Vector2 starting_position;
 } level;
-
-#define MAX_WIDTH 512
-#define MAX_HEIGHT 512
-#define LEVEL_SIZE 20
 
 level *level_load();
 void level_destroy(level *l);
---
+++ src/tiletypes.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#define TILETYPE_NONE 0
+#define TILETYPE_AIR 1
+
+#define TILETYPE_DIRT 2
+#define TILETYPE_DIRT_ROCK 3
+#define TILETYPE_DIRT_OVERHANG 4
+
+#define TILETYPE_SURFACE 5
+#define TILETYPE_GRASS 6
+#define TILETYPE_GRASS_ROCK1 7
+#define TILETYPE_GRASS_ROCK2 8
+
+#define TILETYPE_TREE 9
+#define TILETYPE_TREE_PINE 10
+#define TILETYPE_TREE_DEAD 11
+
+#define TILETYPE_BRICK 12
+#define TILETYPE_DOOR 13
+#define TILETYPE_DOOR_OPEN 14
+#define TILETYPE_STAIRS_UP 15
+#define TILETYPE_STAIRS_DOWN 16
+#define TILETYPE_COBWEB 17
+
+#define NUM_TILE_TYPES 18 // Always one more than the last tiletype
